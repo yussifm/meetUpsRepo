022439902449b474a4ff56e3bbab04952a0500d5
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-05-18 14:09:28 +0000

    second comment

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Todo App'),
+      home: const MyHomePage(title: 'Todo App Coded'),
     );
   }
 }
